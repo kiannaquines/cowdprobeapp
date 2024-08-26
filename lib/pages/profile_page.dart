52c2145e0826e006de48c5c5ccaad86cec45a1bf
kiannaquines <kjgnaquines@gmail.com>
2024-08-26 18:53:07 +0800

    add enhancement to the UI Profile Page
    Signed-off-by: kiannaquines <kjgnaquines@gmail.com>

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:crowdapp/utils/colors.dart';
 import 'package:crowdapp/widgets/appbar_widget.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -36,12 +37,80 @@ class _ProfilePageState extends State<ProfilePage> {
                 color: AppColors.pallete800,
               ),
             )
-          : const Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Kian Naquines')
-                ],
+          : SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Expanded(
+                flex: 3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ClipRRect(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(75.0)),
+                      child: Container(
+                        width: 140,
+                        height: 140,
+                        decoration: BoxDecoration(
+                          color: Colors.brown.shade300,
+                        ),
+                        child: Center(
+                          child: Text(
+                            'KN',
+                            style: GoogleFonts.getFont(
+                              'Poppins',
+                              color: AppColors.backgroundColor,
+                              fontSize: 50.0,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      'Kian G. Naquines',
+                      style: GoogleFonts.getFont(
+                        'Poppins',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.grey600,
+                      ),
+                    ),
+                    Text(
+                      '@kjgnaquines',
+                      style: GoogleFonts.getFont(
+                        'Poppins',
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w400,
+                        color: AppColors.grey800,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 5.0,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.backgroundColor,
+                        overlayColor: AppColors.pallete100,
+                      ),
+                      child: Text(
+                        'Edit Profile',
+                        style: GoogleFonts.getFont(
+                          'Poppins',
+                          color: AppColors.pallete800,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
       floatingActionButton: FloatingActionButton(
