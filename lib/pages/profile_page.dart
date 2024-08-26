9b2972333277479e2839678758c66f020ba7d43a
kiannaquines <kjgnaquines@gmail.com>
2024-08-26 20:09:21 +0800

    added more components in the Profile Page
    Signed-off-by: kiannaquines <kjgnaquines@gmail.com>

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:crowdapp/utils/colors.dart';
 import 'package:crowdapp/widgets/appbar_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:icons_flutter/icons_flutter.dart';
+import 'package:iconsax/iconsax.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -29,98 +31,225 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:
-          const AppBarWidget(title: 'Profile', textColor: AppColors.grey600),
+      appBar: AppBarWidget(
+        title: 'Your Profile',
+        textColor: AppColors.grey600,
+        actions: [
+          IconButton(
+            onPressed: () {
+              debugPrint('Profile Settings');
+            },
+            icon: const Icon(Feather.settings),
+          ),
+          IconButton(
+            onPressed: () {
+              debugPrint('Logout Settings');
+            },
+            icon: const Icon(Feather.log_out),
+          ),
+        ],
+      ),
       body: _isLoading
           ? const Center(
               child: CircularProgressIndicator(
                 color: AppColors.pallete800,
               ),
             )
-          : SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Expanded(
-                flex: 3,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    ClipRRect(
-                      borderRadius:
-                          const BorderRadius.all(Radius.circular(75.0)),
-                      child: Container(
-                        width: 140,
-                        height: 140,
-                        decoration: BoxDecoration(
-                          color: Colors.brown.shade300,
-                        ),
-                        child: Center(
-                          child: Text(
-                            'KN',
-                            style: GoogleFonts.getFont(
-                              'Poppins',
-                              color: AppColors.backgroundColor,
-                              fontSize: 50.0,
-                              fontWeight: FontWeight.w500,
-                            ),
+          : DefaultTabController(
+              length: 2,
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Expanded(
+                  flex: 3,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      CircleAvatar(
+                        radius: 60,
+                        backgroundColor: AppColors.pallete800,
+                        child: Text(
+                          'KN',
+                          style: GoogleFonts.getFont(
+                            'Poppins',
+                            color: AppColors.backgroundColor,
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.w500,
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 10.0,
-                    ),
-                    Text(
-                      'Kian G. Naquines',
-                      style: GoogleFonts.getFont(
-                        'Poppins',
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w600,
-                        color: AppColors.grey600,
+                      const SizedBox(
+                        height: 10.0,
                       ),
-                    ),
-                    Text(
-                      '@kjgnaquines',
-                      style: GoogleFonts.getFont(
-                        'Poppins',
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.w400,
-                        color: AppColors.grey800,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 5.0,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: AppColors.backgroundColor,
-                        overlayColor: AppColors.pallete100,
-                      ),
-                      child: Text(
-                        'Edit Profile',
+                      Text(
+                        'Kian G. Naquines',
                         style: GoogleFonts.getFont(
                           'Poppins',
-                          color: AppColors.pallete800,
-                          fontWeight: FontWeight.w600,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w500,
+                          color: AppColors.grey600,
                         ),
                       ),
-                    ),
-                  ],
+                      Text(
+                        '@kjgnaquines',
+                        style: GoogleFonts.getFont(
+                          'Poppins',
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w500,
+                          color: AppColors.grey800,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5.0,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColors.backgroundColor,
+                          overlayColor: AppColors.pallete100,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                              5,
+                            ),
+                          ),
+                        ),
+                        child: Text(
+                          'Edit Profile',
+                          style: GoogleFonts.getFont(
+                            'Poppins',
+                            color: AppColors.pallete800,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                        child: TabBar(
+                          labelColor: AppColors.pallete800,
+                          unselectedLabelColor: AppColors.grey600,
+                          indicatorColor: AppColors.pallete800,
+                          indicatorSize: TabBarIndicatorSize.tab,
+                          overlayColor: WidgetStateProperty.resolveWith<Color?>(
+                            (Set<WidgetState> states) {
+                              if (states.contains(WidgetState.pressed)) {
+                                return AppColors.pallete800
+                                    .withOpacity(0.1);
+                              }
+                              return null;
+                            },
+                          ),
+                          labelStyle: GoogleFonts.getFont(
+                            'Poppins',
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          tabs: const [
+                            Tab(
+                              text: 'Likes',
+                            ),
+                            Tab(
+                              text: 'Activities',
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: TabBarView(
+                          children: [
+                            SingleChildScrollView(
+                              child: Padding(
+                                padding: const EdgeInsets.all(15.0),
+                                child: Column(
+                                  children: List.generate(6, (index) {
+                                    return Container(
+                                      color: AppColors.backgroundColor,
+                                      margin: const EdgeInsets.symmetric(
+                                          vertical: 4.0),
+                                      child: ListTile(
+                                        splashColor: AppColors.pallete100,
+                                        leading: const Icon(
+                                          Iconsax.clock,
+                                          color: AppColors.pallete700,
+                                        ),
+                                        title: Text(
+                                          'Reference Section',
+                                          style: GoogleFonts.getFont(
+                                            'Poppins',
+                                            color: AppColors.pallete950,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                        trailing: const Icon(
+                                          Feather.more_vertical,
+                                          color: AppColors.pallete800,
+                                        ),
+                                        subtitle: Text(
+                                          'You visited last January 21, 2024',
+                                          style: GoogleFonts.getFont(
+                                            'Poppins',
+                                            color: AppColors.pallete900,
+                                            fontSize: 12.0,
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  }),
+                                ),
+                              ),
+                            ),
+                            SingleChildScrollView(
+                              child: Padding(
+                                padding: const EdgeInsets.all(15.0),
+                                child: Column(
+                                  children: List.generate(6, (index) {
+                                    return Container(
+                                      color: AppColors.backgroundColor,
+                                      margin: const EdgeInsets.symmetric(
+                                          vertical: 4.0),
+                                      child: ListTile(
+                                        splashColor: AppColors.pallete100,
+                                        leading: const Icon(
+                                          Iconsax.clock,
+                                          color: AppColors.pallete700,
+                                        ),
+                                        title: Text(
+                                          'Reference Section',
+                                          style: GoogleFonts.getFont(
+                                            'Poppins',
+                                            color: AppColors.pallete950,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                        trailing: const Icon(
+                                          Feather.more_vertical,
+                                          color: AppColors.pallete800,
+                                        ),
+                                        subtitle: Text(
+                                          'You visited last January 21, 2024',
+                                          style: GoogleFonts.getFont(
+                                            'Poppins',
+                                            color: AppColors.pallete900,
+                                            fontSize: 12.0,
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  }),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: AppColors.pallete700,
-        onPressed: () {},
-        child: const Icon(
-          Icons.camera_alt_outlined,
-          color: Colors.white,
-        ),
-      ),
     );
   }
 }
--- lib/widgets/appbar_widget.dart
+++ lib/widgets/appbar_widget.dart
@@ -4,48 +4,51 @@ import 'package:crowdapp/utils/colors.dart';
 import 'package:icons_flutter/icons_flutter.dart';
 
 class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
-
   final String title;
   final Color textColor;
-
-  @override
-  Size get preferredSize => const Size.fromHeight(60.0);
+  final List<Widget> actions;
 
   const AppBarWidget({
     super.key,
     required this.title,
     required this.textColor,
-  });
+    List<Widget>? actions,
+  }) : actions = actions ??
+            const [
+              IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Feather.grid,
+                  color: AppColors.grey600,
+                ),
+              ),
+              IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Feather.camera,
+                  color: AppColors.grey600,
+                ),
+              ),
+            ];
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60.0);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       title: Text(
         title,
-        style: GoogleFonts.getFont('Poppins',
+        style: GoogleFonts.getFont(
+          'Poppins',
           color: textColor,
-          fontWeight: FontWeight.w600,
+          fontWeight: FontWeight.w500,
           fontSize: 25.0,
         ),
       ),
       toolbarHeight: 60,
       forceMaterialTransparency: true,
-      actions: [
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            Feather.grid,
-            color: AppColors.grey600,
-          ),
-        ),
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            Feather.camera,
-            color: AppColors.grey600,
-          ),
-        ),
-      ],
+      actions: actions,
     );
   }
 }
